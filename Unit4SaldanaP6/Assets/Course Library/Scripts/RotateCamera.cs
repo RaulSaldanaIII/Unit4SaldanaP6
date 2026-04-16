9e203e49ee645b39f5a100e0a9bd4caae522aecf
RaulSaldanaIII <756853@student.dusd.net>
2026-04-16 13:43:15 -0700

    Completed 4.2
    4/16/26

--- Unit4SaldanaP6/Assets/Course Library/Scripts/RotateCamera.cs
+++ Unit4SaldanaP6/Assets/Course Library/Scripts/RotateCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class RotateCamera : MonoBehaviour
 {
     public float rotationSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
